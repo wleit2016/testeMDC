5c7046ac830105d2e7ce57803a51b63fba3131f2
wleit2016 <wenderleite2008@gmail.com>
2023-03-20 12:01:03 -0300

    foi adiciona no arquivo principal a logica para decompor os dois numeros em fatores primos, usando o laco de repeticao while. Alem disso foi criado uma funcao exibir, para exibir o resultado na tela

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-from Calculos import ePrimo
 from Calculos import proximoNumeroPrimo
 
+def exibir(numero1, numero2, primo):
+    print(numero1, "-", numero2, "|", primo)
+
 if __name__ == '__main__':
-    numero1 = int(input("Digite um numero: "))
+    numeroPrimo = 2
+    repita = True
+    listaNumerosPrimos = []
 
-    print(ePrimo(numero1))
+    numero1TXT = input("Digite um numero: ")
+    numero2TXT = input("Digite outro numero: ")
 
-    print(proximoNumeroPrimo(numero1))
+    numero1 = int(numero1TXT)
+    numero2 = int(numero2TXT)
+
+    while repita:
+        if numero1 % numeroPrimo == 0 and numero2 % numeroPrimo == 0:
+            exibir(numero1, numero2, numeroPrimo)
+            listaNumerosPrimos.append(numeroPrimo)
+            numero1 = int(numero1 / numeroPrimo)
+            numero2 = int(numero2 / numeroPrimo)
+        else:
+            if numero1 % numeroPrimo == 0:
+                exibir(numero1, numero2, numeroPrimo)
+                numero1 = int(numero1 / numeroPrimo)
+            else:
+                if numero2 % numeroPrimo == 0:
+                    exibir(numero1, numero2, numeroPrimo)
+                    numero2 = int(numero2 / numeroPrimo)
+                else:
+                    numeroPrimo = proximoNumeroPrimo(numeroPrimo)
+
+        if numero1 == 1 and numero2 == 1:
+            repita = False
